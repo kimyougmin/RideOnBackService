b70a49e0a3bfb008c39bd1d597cefe1188f23e34
kimyougmin <dudals896@gmail.com>
2025-06-24 16:00:47 +0900

    feat: 로그인 Getter 추가

--- src/main/java/com/ll/rideon/domain/users/dto/UserLoginResponseDto.java
+++ src/main/java/com/ll/rideon/domain/users/dto/UserLoginResponseDto.java
@@ -1,9 +1,10 @@
 package com.ll.rideon.domain.users.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserLoginResponseDto {
     private String token;
-
-    public UserLoginResponseDto(String token) {
-        this.token = token;
-    }
 }
